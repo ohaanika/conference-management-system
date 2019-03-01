7db61d1cd321f1a430d66d7c334e5510e06f92cd
Aanika Rahman <aanika.rahman@mail.mcgill.ca>
2019-03-01 13:43:02 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,10 +7,18 @@ FROM (Attendee JOIN (SELECT aID FROM Delegate WHERE university="McGill") ON Atte
 SELECT firstName, lastName, emailAddress
 FROM (Attendee JOIN (SELECT aID FROM (Sponsor) WHERE companyID="Kinaxis") ON Attendee.aID=Sponsor.aID))
 
+--
+SELECT description,deadlineTime 
+FROM Task
+WHERE (status="false"&&deadlineDate="2019-01-09")
 
-SELECT description,deadlineTime FROM Task WHERE (status="false"&&deadlineDate="2019-01-09")
+--
+SELECT title 
+FROM Event 
+WHERE locationID = NULL
 
-SELECT title FROM Event WHERE locationID = NULL
-
-SELECT hID FROM HotelBooking WHERE HotelBooking.capacity != 0
+--
+SELECT hID 
+FROM HotelBooking
+WHERE HotelBooking.capacity != 0
 
