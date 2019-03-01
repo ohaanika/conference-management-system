ce31bb64f97957e2b59b32039d4692e14e4efdc7
Reem Madkour <reem.madkour@mail.mcgill.ca>
2019-02-28 22:16:01 -0500

    queries for #4

---
+++ queries.sql
@@ -0,0 +1,19 @@
+
+SELECT firstname,lastname,email
+FROM (Attendee 
+JOIN(SELECT aID FROM Delegate WHERE university="McGill")
+ON  Attendee.aID=Delegate.aID))
+
+
+SELECT firstname,lastname,email
+FROM (Attendee 
+JOIN(SELECT aID FROM (Sponsor) WHERE companyID="Kinaxis")
+ON  Attendee.aID=Sponsor.aID))
+
+
+SELECT description,deadlineTime FROM Task WHERE (status="false"&&deadlineDate="2019-01-09")
+
+SELECT title FROM Event WHERE locationID = NULL
+
+SELECT hID FROM HotelBooking WHERE HotelBooking.capacity != 0
+
