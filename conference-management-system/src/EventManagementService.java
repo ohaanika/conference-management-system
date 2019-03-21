4fcfdf68c2171a45346e8a44fc4c9ddfc420a3d8
Aanika Rahman <aanika.rahman@mail.mcgill.ca>
2019-03-21 14:45:41 -0400

    edits to event management service

--- conference-management-system/src/EventManagementService.java
+++ conference-management-system/src/EventManagementService.java
@@ -6,8 +6,8 @@ public class EventManagementService {
      *
      * @param title the title of the event
      * @param type the type of event
-     * @param delegateId an attendee being interviewd, null otherwise
-     * @param sponsorId a sponsor which is interviewing an attendee, null otherwise
+     * @param delegateId the delegate being interviewed, null otherwise
+     * @param sponsorId the sponsor interviewing an attendee, null otherwise
      */
     public void createEvent(String title, String type, String delegateId, String sponsorId) {
         // An entry in the table is created for any event of any type due to ISA relationship.
