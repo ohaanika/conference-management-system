6e563b461ac0400a4b902e92919baf4aa1271a97
RamshaIjaz <32585928+RamshaIjaz@users.noreply.github.com>
2019-03-23 11:48:23 -0400

    Update AttendeeManagementService.java

--- conference-management-system/src/AttendeeManagementService.java
+++ conference-management-system/src/AttendeeManagementService.java
@@ -23,7 +23,7 @@ public class AttendeeManagementService {
 			UUID aid = UUID.randomUUID();
 			connection.prepareStatement("INSERT INTO Attendee VALUES (" + aid + "," + firstName + "," + lastName + ","
 					+ emailAddress + "," + shirtCut + "," + shirtSize + "," + dietaryRestrictions + ")");
-			ConnectionManager.closeConnection();
+			connection.close();
 		} catch (SQLException e) {
 			System.err.println("msg: " + e.getMessage() +"code: "+ e.getErrorCode() +"state: "+ e.getSQLState());
 		}
@@ -33,7 +33,15 @@ public class AttendeeManagementService {
 
 		
 	}
-
+    public void getAttendee(UUID aid) {
+    	Connection connection = ConnectionManager.getConnectionInstance();
+    	try {
+			connection.("SELECT * FROM Attendee WHERE aid ");
+			connection.close();
+		} catch (SQLException e) {
+			System.err.println("msg: " + e.getMessage() +"code: "+ e.getErrorCode() +"state: "+ e.getSQLState());
+		}
+//for debugging
     public void printStr(String type, String firstName, String lastName, String emailAddress, String shirtCut, String shirtSize, String dietaryRestrictions, String major, String university, String province, String role, String company) {
     	System.out.println("INSERT INTO Attendee VALUES (" +  UUID.randomUUID()+ "," + firstName + "," + lastName + "," + emailAddress
     		+ "," + shirtCut + "," + shirtSize + "," + dietaryRestrictions + ")");
